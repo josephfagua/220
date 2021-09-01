5a0883d029b27f7e80de681e901c81b8d96695af
josephfagua <faguaja@g.cofc.edu>
2021-09-01 16:52:20 -0400

    completed Hw1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,2 +1,15 @@
+"""
+Joseph Fagua
+hello.py
+
+Problem: Write a Python program, test a python program, and submit a python program
+
+Certificate of Authenticity: I certify that this assignment is entirely my work.
+"""
+
+
 def main():
     print("hello, world!")
+
+
+main()
