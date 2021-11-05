a1b5d61e70e82704ea5050c78e9633b4d05adc48
josephfagua <faguaja@g.cofc.edu>
2021-11-05 16:18:05 -0400

    Completed HW8

--- assignments/hw8/bumper.py
+++ assignments/hw8/bumper.py
@@ -6,53 +6,66 @@ Purpose: Develop a python program that creates a window that has 2 circles bounc
 Certification of Authenticity:
 I certify that this work is entirely my own.
 """
-from graphics import GraphWin, Point, Circle, color_rgb
 from random import randint
 from time import sleep
-import math
+from math import sqrt
+from graphics import GraphWin, Point, Circle, color_rgb
 
 
 def get_random_color():
-    cr = randint(0, 255)
-    cg = randint(0, 255)
-    cb = randint(0, 255)
-    return color_rgb(cr, cg, cb)
+    color_r = randint(0, 255)
+    color_g = randint(0, 255)
+    color_b = randint(0, 255)
+    return color_rgb(color_r, color_g, color_b)
 
 
 def get_random(move_amount):
-    negMove = -1 * int(move_amount)
-    ranMove = randint(negMove, move_amount)
-    return ranMove
+    neg_move = -1 * int(move_amount)
+    ran_move = randint(neg_move, move_amount)
+    return ran_move
+
+
+def did_collide(circle1, circle2):
+    cords_cb1 = circle1.getCenter()
+    cords_cb2 = circle2.getCenter()
+    cx_1 = int(cords_cb1.getX())
+    cy_1 = int(cords_cb1.getY())
+    cx_2 = int(cords_cb2.getX())
+    cy_2 = int(cords_cb2.getY())
+    distance_points = sqrt((cx_2 - cx_1) ** 2 + (cy_2 - cy_1) ** 2)
+    return distance_points < circle1.getRadius() + circle2.getRadius()
+
+
+def hit_horizontal(circle, graphwin):
+    radius = circle.getRadius()
+    ceiling = graphwin.getHeight() - radius
+    return circle.getCenter().getY() <= radius or circle.getCenter().getY() >= ceiling
+
+
+def hit_vertical(circle, graphwin):
+    radius = circle.getRadius()
+    right_wall = graphwin.getWidth() - radius
+    return circle.getCenter().getX() <= radius or circle.getCenter().getX() >= right_wall
 
 
 def main():
     win = GraphWin("Bumper Cars", 600, 900)
     win.setBackground(get_random_color())
 
-    x1 = 300
-    y1 = 450
-    x2 = 250
-    y2 = 300
+    x_1 = 300
+    y_1 = 450
+    x_2 = 250
+    y_2 = 300
     dx1 = get_random(15)
     dy1 = get_random(15)
     dx2 = get_random(12)
     dy2 = get_random(12)
-    r1 = 40
-    r2 = 30
-    circle_ball = Circle(Point(x1, y1), r1)
+    r_1 = 40
+    r_2 = 30
+    circle_ball = Circle(Point(x_1, y_1), r_1)
     circle_ball.draw(win)
-    circle_ball2 = Circle(Point(x2, y2), r2)
+    circle_ball2 = Circle(Point(x_2, y_2), r_2)
     circle_ball2.draw(win)
-
-    floor = r1
-    ceiling = win.getHeight() - r1
-    left_wall = r1
-    right_wall = win.getWidth() - r1
-    floor2 = r2
-    ceiling2 = win.getHeight() - r2
-    left_wall2 = r2
-    right_wall2 = win.getWidth() - r2
-
     circle_ball.setFill(get_random_color())
     circle_ball.setOutline(get_random_color())
     circle_ball2.setFill(get_random_color())
@@ -63,27 +76,19 @@ def main():
         sleep(0.00000000000000001)
         circle_ball2.move(dx2, dy2)
         sleep(0.0001)
-        cordsCB1 = circle_ball.getCenter()
-        cordsCB2 = circle_ball2.getCenter()
-        cx1 = int(cordsCB1.getX())
-        cy1 = int(cordsCB1.getY())
-        cx2 = int(cordsCB2.getX())
-        cy2 = int(cordsCB2.getY())
-        distance2Points = math.sqrt((cx2 - cx1)**2 + (cy2 - cy1)**2)
-        if circle_ball.getCenter().getY() <= floor or circle_ball.getCenter().getY() >= ceiling:
+        if hit_horizontal(circle_ball, win):
             dy1 = -dy1
-        elif circle_ball.getCenter().getX() <= left_wall or circle_ball.getCenter().getX() >= right_wall:
+        elif hit_vertical(circle_ball, win):
             dx1 = -dx1
-        elif circle_ball2.getCenter().getY() <= floor2 or circle_ball2.getCenter().getY() >= ceiling2:
+        elif hit_horizontal(circle_ball2, win):
             dy2 = -dy2
-        elif circle_ball2.getCenter().getX() <= left_wall2 or circle_ball2.getCenter().getX() >= right_wall2:
+        elif hit_vertical(circle_ball2, win):
             dx2 = -dx2
-        elif distance2Points < r1 + r2:
+        elif did_collide(circle_ball, circle_ball2):
             dy1 = -dy1
             dx1 = -dx1
             dx2 = -dx2
             dy2 = -dy2
-
 
 if __name__ == '__main__':
     main()
