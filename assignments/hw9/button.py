478d160a7de430aaebbf0e6a1f341159f92e7be8
Joseph Fagua <faguaja@g.cofc.edu>
2021-11-18 18:16:47 -0500

    Hw 9 Completed

--- assignments/hw9/button.py
+++ assignments/hw9/button.py
@@ -1,41 +1,50 @@
-from graphics import *
+"""
+Name: Joseph Fagua
+button.py
+Purpose: Develop a python program that creates a button class to be used in three_door_game.py
+
+Certification of Authenticity:
+I certify that this work is entirely my own.
+"""
+from graphics import Text
 
 
 class Button:
+    """This class creates a Button with a label at the center"""
     def __init__(self, rectangle_shape, string_label):
+        """This constructs the button class"""
         self.shape = rectangle_shape
-        self.label = string_label
+        self.text = string_label
 
     def get_label(self):
         """This method returns the text of the button"""
-        return self.label
+        return self.text
 
     def draw(self, win):
         """This method draws the button with text on it to the window"""
-        p1 = self.shape.getCenter()
-        self.label = Text(p1, self.label)
+        p_1 = self.shape.getCenter()
+        self.text = Text(p_1, self.text)
         self.shape.draw(win)
-        self.label.draw(win)
+        self.text.draw(win)
 
     def undraw(self):
         """This method undraws the button with text on it to the window"""
         self.shape.undraw()
-        self.label.undraw()
+        self.text.undraw()
 
     def is_clicked(self, point):
         """This method checks to see if the mouse click in the region"""
-        p1 = self.shape.getP1()
-        p2 = self.shape.getP2()
-        w = abs(p1.getX() - p2.getX())
-        h = abs(p1.getY() - p2.getY())
-        x = point.getX()
-        y = point.getY()
-        dx = abs(self.shape.getCenter().getX() - x)
-        dy = abs(self.shape.getCenter().getY() - y)
-        if dx <= w/2 and dy <= h/2:
+        p_1 = self.shape.getP1()
+        p_2 = self.shape.getP2()
+        width = abs(p_1.getX() - p_2.getX())
+        height = abs(p_1.getY() - p_2.getY())
+        x_1 = point.getX()
+        y_1 = point.getY()
+        d_x = abs(self.shape.getCenter().getX() - x_1)
+        d_y = abs(self.shape.getCenter().getY() - y_1)
+        if d_x <= width/2 and d_y <= height/2:
             return True
-        else:
-            return False
+        return False
 
     def color_button(self, color):
         """This method changes the color of the buttons to either green or red"""
@@ -43,4 +52,4 @@ class Button:
 
     def set_label(self, label):
         """This method changes the text of the doors to given string"""
-        self.label.setText(label)
+        self.text.setText(label)
--- assignments/hw9/three_door_game.py
+++ assignments/hw9/three_door_game.py
@@ -1,28 +1,33 @@
-from button import Button
-from random import choice, randrange
+"""
+Name: Joseph Fagua
+three_door_game.py
+Purpose: Develop a python program that uses the button class to create a game
+
+Certification of Authenticity:
+I certify that this work is entirely my own.
+"""
+from random import randrange
 from graphics import GraphWin, Rectangle, Point, Text
+from button import Button
 
 
 def three_door_game():
     win = GraphWin("Three Door Game", 350, 250)
     win.setCoords(0, 0, 30, 50)
     top_message = Text(Point(15, 40), "I have a secret door")
-    """Doors"""
     box1 = Rectangle(Point(2.5, 15), Point(8.5, 26))
     box2 = Rectangle(Point(12, 15), Point(18, 26))
     box3 = Rectangle(Point(21.5, 15), Point(27.5, 26))
     door1 = Button(box1, "Door 1")
     door2 = Button(box2, "Door 2")
     door3 = Button(box3, "Door 3")
-    """Drawing Doors and Text"""
     top_message.draw(win)
     door1.draw(win)
     door2.draw(win)
     door3.draw(win)
-    bot_message = Text(Point(15, 2), "click to choose my door")
+    bot_message = Text(Point(15, 2), "click to choose door")
     bot_message.draw(win)
     option = randrange(1, 4)
-
     click = win.getMouse()
     if door1.is_clicked(click):
         if option == 1:
@@ -33,7 +38,26 @@ def three_door_game():
             door1.color_button("red")
             top_message.setText("You Lose!")
             bot_message.setText("Door " + str(option) + " is the secret door")
+    if door2.is_clicked(click):
+        if option == 2:
+            door2.color_button("green")
+            top_message.setText("You Win!")
+            bot_message.setText("click to close")
+        if option != 2:
+            door2.color_button("red")
+            top_message.setText("You Lose!")
+            bot_message.setText("Door " + str(option) + " is the secret door")
+    if door3.is_clicked(click):
+        if option == 3:
+            door3.color_button("green")
+            top_message.setText("You Win!")
+            bot_message.setText("click to close")
+        if option != 3:
+            door3.color_button("red")
+            top_message.setText("You Lose!")
+            bot_message.setText("Door " + str(option) + " is the secret door")
     win.getMouse()
+
 
 if __name__ == '__main__':
     three_door_game()
