7424e79b24cc5ca872469a57fe3c303d6a24f881
josephfagua <faguaja@g.cofc.edu>
2021-12-02 16:02:45 -0500

    Hw 10 update

--- assignments/hw10/sales_force.py
+++ assignments/hw10/sales_force.py
@@ -14,12 +14,31 @@ class SalesForce:
         self.sales_people = []
 
     def add_data(self, file_name):
-        file = open(file_name, "r")
-        for line in file:
+        infile = open(file_name, "r")
+        for line in infile:
             self.sales_people += line
 
-    def top_seller(self):
+    def quota_report(self, quota):
         pass
 
+    def top_seller(self):
+        top = []
+        for i in range(len(self.sales_people)):
+            highest = self.sales_people[i].get_sales()
+            pos = i
+            for j in range(i, len(self.sales_people)):
+                if self.sales_people[j].get_sales() > highest:
+                    highest = self.sales_people[j].get_sales()
+                    pos = j
+            self.sales_people[i], self.sales_people[pos] = self.sales_people[pos], self.sales_people[i]
+        top.append(self.sales_people[0])
+        for i in range(1, len(self.sales_people)):
+            if self.sales_people[i].total_sales() == top[0].total_sales():
+                top.append(self.sales_people[i])
+        return top
+
     def individual_sales(self, employee_id):
-        pass
+        for person in self.sales_people:
+            if person.get_id() == employee_id:
+                return person
+        return None
